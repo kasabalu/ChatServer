82a5606b35f20a66058fcbb6f1803a855fa932ac
bkasa724 <balakrishna_kasa@comcast.com>
2022-05-04 15:57:29 -0400

    application with validations

--- chatServerUsingSocket/chatServerUsingFlask/application.py
+++ chatServerUsingSocket/chatServerUsingFlask/application.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
+from wtform_fileds import *
 
 app = Flask(__name__)
 app.secret_key= "mysecret"
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    reg_form = RegistrationForm()
+    if reg_form.validate_on_submit():
+        return "success"
+
+    return render_template("index.html",form=reg_form)
 
 
 if __name__ == "__main__":
